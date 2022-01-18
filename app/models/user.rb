90c1ffe9e777c54efb2bcca941ab8341df10cc7f
haruska <2803+haruska@users.noreply.github.com>
2022-01-17 20:28:50 -0600

    user scaffolding

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: /.+@.+\..+/
 end
