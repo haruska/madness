072433cd86e60a4a64a6678efb51a54ede751c9f
haruska <2803+haruska@users.noreply.github.com>
2022-02-10 08:02:26 -0600

    display tournament

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -4,4 +4,12 @@ module TournamentsHelper
   def tournament
     Tournament.field64
   end
+
+  def date_range_string(start_date, end_date)
+    date_str = start_date.strftime('%b %-d')
+
+    date_str += "-#{end_date.strftime('%-d')}" if end_date > start_date
+
+    date_str
+  end
 end
