7b437d31f9b9a320ef71d8a14232d1e4df4ae662
haruska <2803+haruska@users.noreply.github.com>
2022-02-16 17:11:19 -0600

    update gems and variables in scss

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include Pundit
+  include Pundit::Authorization
   include Passwordless::ControllerHelpers
 
   helper_method :current_user
