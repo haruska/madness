b83bec82e096b095ff73d9562a7185036fcd2448
haruska <2803+haruska@users.noreply.github.com>
2022-02-21 09:18:43 -0600

    add brackets dashboard

--- app/dashboards/bracket_dashboard.rb
+++ app/dashboards/bracket_dashboard.rb
@@ -2,7 +2,7 @@
 
 require 'administrate/base_dashboard'
 
-class UserDashboard < Administrate::BaseDashboard
+class BracketDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
   # a hash that describes the type of each of the model's fields.
   #
@@ -10,11 +10,12 @@ class UserDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
+    user: Field::BelongsTo,
     id: Field::Number,
     name: Field::String,
-    email: Field::String,
-    admin: Field::Boolean,
-    brackets: Field::HasMany,
+    tree_decisions: Field::String.with_options(searchable: false),
+    tie_breaker: Field::Number,
+    paid: Field::Boolean,
     created_at: Field::DateTime,
     updated_at: Field::DateTime
   }.freeze
@@ -26,30 +27,33 @@ class UserDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
     id
+    user
     name
-    email
-    brackets
+    paid
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
+    user
     id
     name
-    email
-    admin
+    tree_decisions
+    tie_breaker
+    paid
     created_at
     updated_at
-    brackets
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
+    user
     name
-    email
-    admin
+    tree_decisions
+    tie_breaker
+    paid
   ].freeze
 
   # COLLECTION_FILTERS
@@ -64,10 +68,10 @@ class UserDashboard < Administrate::BaseDashboard
   #   }.freeze
   COLLECTION_FILTERS = {}.freeze
 
-  # Overwrite this method to customize how users are displayed
+  # Overwrite this method to customize how brackets are displayed
   # across all pages of the admin dashboard.
   #
-  def display_resource(user)
-    user.name
-  end
+  # def display_resource(bracket)
+  #   "Bracket ##{bracket.id}"
+  # end
 end
