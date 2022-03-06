95106083d90b1a784e6652fd800277afd95d6a4c
haruska <2803+haruska@users.noreply.github.com>
2022-03-06 09:58:54 -0600

    use dot env and env vars instead of Rails.credentials

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@
 Rails.logger.debug 'Running seeds.rb...'
 
 # ensure admin user
-admin_name = Rails.application.credentials.admin_name!
-admin_email = Rails.application.credentials.admin_email!
+admin_name = ENV['ADMIN_NAME']
+admin_email = ENV['ADMIN_EMAIL']
 User.find_or_create_by!(name: admin_name, email: admin_email, admin: true)
 
 Tournament.field_64 || Tournament.create!(
