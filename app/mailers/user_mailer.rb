f456c74800dca48669e8e5fd8d6a9ed5002c738b
haruska <2803+haruska@users.noreply.github.com>
2022-03-06 13:44:06 -0600

    improve login email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,9 @@
 
 class UserMailer < ApplicationMailer
   def validate_email(user, url)
+    subject_host = ENV['HOST'] || 'pool-madness.com'
     @user = user
     @url  = url
-    mail to: @user.email, subject: 'Sign in into pool-madness.com'
+    mail from: "Pool Madness <#{ENV['EMAIL_SENDER']}>", to: @user.email, subject: "Sign in to #{subject_host}"
   end
 end
