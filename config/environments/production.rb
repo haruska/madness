56d9eefe2ec7e3d453a1606de53e6529da9baff7
haruska <2803+haruska@users.noreply.github.com>
2022-03-17 12:07:48 -0500

    add a reconnect attempt in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,10 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
+  config.cache_store = :redis_cache_store, {
+    url: ENV['REDIS_URL'],
+    reconnect_attempts: 1
+  }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
