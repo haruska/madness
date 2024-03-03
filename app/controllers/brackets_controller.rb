16cd3033d3c3a442350aa3170a71c076ec94c78e
haruska <2803+haruska@users.noreply.github.com>
2024-03-03 11:19:23 -0600

    break up into layouts and partials

--- app/controllers/brackets_controller.rb
+++ app/controllers/brackets_controller.rb
@@ -1,10 +1,12 @@
 class BracketsController < ApplicationController
+  layout 'static'
   def my_brackets
     if Tournament.field_64.started?
       redirect_to '/brackets'
       return
     end
 
+    @title = 'My Brackets'
     @brackets = current_user.brackets
   end
 end
