c2b55feeffcea89ced4362c8860fba0b1556bea2
haruska <2803+haruska@users.noreply.github.com>
2024-03-08 09:25:39 -0600

    move bracket render into shared partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,43 @@ module ApplicationHelper
   def tournament
     @tournament ||= Tournament.field_64
   end
+
+  def teams
+    @teams ||= tournament.teams
+  end
+
+  def tournament_react_props(bracket = nil)
+    props = {
+      tournament: {
+        gameDecisions: tournament.game_decisions.to_s,
+        gameMask: tournament.game_mask.to_s,
+        rounds: tournament.rounds.map do |round|
+          {
+            name: round.name,
+            number: round.number,
+            startDate: round.start_date.iso8601,
+            endDate: round.end_date.iso8601,
+            regions: round.regions
+          }
+        end
+      },
+      teams: teams.map do |team|
+        {
+          startingSlot: team.starting_slot,
+          seed: team.seed,
+          name: team.name
+        }
+      end
+    }
+
+    if bracket
+      props[:bracket] = {
+        name: bracket.name,
+        gameDecisions: bracket.game_decisions.to_s,
+        gameMask: 18_446_744_073_709_551_614.to_s
+      }
+    end
+
+    props
+  end
 end
