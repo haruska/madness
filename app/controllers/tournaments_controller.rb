b99e792ac3f2c1150f8a749016bca02a699536ba
haruska <2803+haruska@users.noreply.github.com>
2024-03-09 15:20:54 -0600

    add update action for Tournament

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -3,4 +3,17 @@
 class TournamentsController < ApplicationController
   layout 'static'
   def show; end
+
+  def update
+    tournament = Tournament.field_64
+    authorize tournament
+    tournament.update!(tournament_params)
+    redirect_to tournament_path
+  end
+
+  protected
+
+  def tournament_params
+    params.require(:tournament).permit(:game_decisions, :game_mask)
+  end
 end
--- app/javascript/components/EditTournament.tsx
+++ app/javascript/components/EditTournament.tsx
@@ -1,6 +1,5 @@
-import React, { FormEvent, useState } from 'react'
+import React, { useState } from 'react'
 import { Team, Tournament as ITournament } from 'objects/TournamentTypes'
-import { ErrorFlash } from 'components/forms/ErrorFlash'
 import { Tournament } from 'components/Tournament'
 import { Dialog } from 'components/Dialog'
 
@@ -15,9 +14,7 @@ export const EditTournament = ({
     BigInt(tournament.gameDecisions),
     BigInt(tournament.gameMask),
   ])
-  const [errors, setErrors] = useState(null)
   const [showDiscardDialog, setShowDiscardDialog] = useState(false)
-
   const [gameDecisions, gameMask] = gameDecisionsMask
 
   const handleSlotClick = (slotId: number, choice: number) => {
@@ -37,30 +34,6 @@ export const EditTournament = ({
     setGameDecisionsMask([decisions, mask])
   }
 
-  // const handleUpdateCompleted = (
-  //   response: UpdateTournamentMutation$data,
-  //   errors: MutationErrors
-  // ) => {
-  //   const allErrors = errors || response.updateTournament.errors
-  //   if (allErrors?.length !== 0) {
-  //     setErrors(allErrors)
-  //   } else {
-  //     window.location.href = '/'
-  //   }
-  // }
-
-  const handleDone = (event: FormEvent<HTMLFormElement>) => {
-    //   event.preventDefault()
-    //   UpdateTournamentMutation.commit(
-    //     {
-    //       gameMask: gameMask.toString(),
-    //       gameDecisions: gameDecisions.toString(),
-    //     },
-    //     handleUpdateCompleted
-    //   )
-    window.location.href = '/'
-  }
-
   // cancel
   const handleDiscard = () => {
     setShowDiscardDialog(true)
@@ -72,18 +45,22 @@ export const EditTournament = ({
 
   const handleConfirmDiscard = () => {
     setShowDiscardDialog(false)
-    window.location.href = '/'
+    window.location.href = '/tournament'
   }
 
+  const authenticityToken = document
+    .querySelector('meta[name="csrf-token"]')
+    .getAttribute('content')
+
   return (
-    <div className="edit-tournament-container">
+    <>
       <Dialog
         isOpen={showDiscardDialog}
         message="You will lose your changes. Are you sure you want to proceed?"
         onConfirm={handleConfirmDiscard}
         onCancel={handleCancelDiscard}
       />
-      <h2>Editing Tournament</h2>
+
       <Tournament
         bracket={{
           name: 'tournament',
@@ -94,13 +71,17 @@ export const EditTournament = ({
         teams={teams}
         onSlotClick={handleSlotClick}
       />
-      <form className="edit-tournament-form" onSubmit={handleDone}>
-        {errors ? <ErrorFlash errors={errors} objectType={'Tournament'} /> : null}
+
+      <form className="edit-tournament-form" action="/tournament" method="POST">
+        <input name="_method" type="hidden" value="put" />
+        <input name="authenticity_token" type="hidden" value={authenticityToken} />
+        <input name="torunament[game_decisions]" type="hidden" value={gameDecisions.toString()} />
+        <input name="tournament[game_mask]" type="hidden" value={gameMask.toString()} />
         <input className="button left-button" type="submit" name="commit" value="Done" />
         <div className="button danger" onClick={handleDiscard}>
           Discard
         </div>
       </form>
-    </div>
+    </>
   )
 }
