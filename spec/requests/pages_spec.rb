522fc1d5d3293e052bad32aeb16d49382a428bcc
haruska <2803+haruska@users.noreply.github.com>
2025-03-10 17:19:33 -0500

    flesh out material design menu

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -12,22 +12,5 @@ RSpec.describe PagesController, type: :request do
     it 'responds successfully with an HTTP 200 status code' do
       expect(response).to have_http_status(:ok)
     end
-
-    it 'displays the title "Rules and Scoring"' do
-      expect(response.body).to include('Rules and Scoring')
-    end
-
-    it 'displays the correct prize percentages' do
-      expect(response.body).to include('1st')
-      expect(response.body).to include('50%')
-      expect(response.body).to include('2nd')
-      expect(response.body).to include('20%')
-      expect(response.body).to include('3rd')
-      expect(response.body).to include('15%')
-      expect(response.body).to include('4th')
-      expect(response.body).to include('10%')
-      expect(response.body).to include('5th')
-      expect(response.body).to include('5%')
-    end
   end
 end
